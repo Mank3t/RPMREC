d37c0c50cc8ea1484fb8bbf659da959abd608df3
Mank3t <163306315+Mank3t@users.noreply.github.com>
2024-10-31 18:00:14 +0300

    Add files via upload

---
+++ GKDorogRosPAges/GKDorogRosPAges/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GKDorogRosPAges
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            NavigWin.Content = new CalendarPage();
+        }
+
+        private void CalendButtOp_Click(object sender, RoutedEventArgs e)
+        {
+
+            NavigWin.Content = new CalendarPage();
+
+        }
+
+        private void IerarhyButtOp_Click(object sender, RoutedEventArgs e)
+        {
+            NavigWin.Content = new Ierarhy();
+        }
+    }
+}
